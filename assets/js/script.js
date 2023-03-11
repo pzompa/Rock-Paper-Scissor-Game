31fa267a0e8c6f2d9d6abfee98fb404f150b7535
Pema Zompa <pemazompa@hotmail.com>
2023-03-11 10:38:42 +0000

    Define variables and get all the elements from DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,22 @@ function runGame () {
     document.getElementById('result').style.display = 'none';
     document.getElementById('reset').style.display = 'none';
 }
+/**
+ * Creat variables and get all the relevant elements
+ */
+const userChoiceDisplay = document.getElementById('user-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('img')
+const messageBoardDisplay = document.getElementById('message-board')
+const resultMessageDisplay = document.getElementById('result-message')
+const youCounterDisplay = document.getElementById('you-counter')
+const computerCounterDisplay = document.getElementById('computer-counter')
+
+let reason
+let userChoice
+let computerChoice
+let result
+let resultMessage
+let youCounter
+let computerCounter
